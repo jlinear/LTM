f5c1f5b4ba121540f662ecddfb149ee6088ec9bf
jlinear <jyang9@nd.edu>
2018-07-31 15:17:31 -0400

    initial code script

---
+++ code/LTM.py
@@ -0,0 +1,14 @@
+def LTM(F, C, beta, alpha0, alpha1, burnin, maxit, sample_size):
+	#### Initialization
+	for f in F:
+		dhj = 1
+
+	#### Sampling
+
+		#### Sample t_f from conditional distribution
+
+		#### Calculate expectation of t_f
+
+
+if __name__ == '__main__':
+	rawdb, F, C =  generate_tables("data_post_CP.txt")
